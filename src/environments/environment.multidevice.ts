5fd2abed0f90aff4a4314e18dcd27b35dc976b05
Gracia Marca <marcaurteagam@gmail.com>
2024-12-09 15:30:01 -0500

    feat: Se agrega nuevo enviroment para ejecutar el proyecto en multiples dispositivos

---
+++ src/environments/environment.multidevice.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  appName: 'SOFT CLEAN',
+  isTest: false,
+  nameSystem: 'SOFT CLEAN',
+  authAngular:{
+    username:'s_soft_clean',
+    password:'Gracia1234'
+  },
+  urlApiMicroservices: {
+    domain: 'http://192.168.18.18:1515',
+  },
+  apiConnectId: '',
+  urlApiIp: 'https://api.ipify.org/?format=json',
+  passwordDefault: 'Epicsbot1234@',
+  version:'v1.0.0'
+};
